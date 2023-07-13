b15e6852c7115394803b357b331c1ee4a8543c4f
NicolasKAmorim <nicolaskamorim@outlook.com>
2023-07-13 18:30:46 -0300

    novos ex

--- aula11/ex008.js
+++ aula11/ex008.js
@@ -1,1 +1,5 @@
-console.log('console funcionou corretamente')
+var vel=50
+console.log(`A velocidade do seu carro é ${vel}`)
+if(vel > 60){ //condição simples
+    console.log(`Voce ultrapssou o limite de velocidade, MULTADO!`)
+}
---
+++ aula11/ex009.js
@@ -0,0 +1,7 @@
+var pais ='estados unidos'
+console.log(`vivendo em ${pais}`)
+if(pais == 'brasil'){
+    console.log('Brasileiro')
+} else {
+    console.log('estrangeiro!')
+}
