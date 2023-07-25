fae32f910b342911ae4a2502fec5ef3933597689
NicolasKAmorim <nicolaskamorim@outlook.com>
2023-07-25 12:54:58 -0300

    att

--- aula14ex2/script.js
+++ aula14ex2/script.js
@@ -1,7 +1,7 @@
 function tabuada(){
     var num = document.getElementById('txtn')
     var tab = document.getElementById('seltab')
-    if (num.Value.length == 0) {
+    if (num.Value == 0) {
         window.alert('digite o numero')
     } else {
         var n = Number(num.value)
